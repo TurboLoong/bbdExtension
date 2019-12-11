2f2e9953d4182a3a84450024ff89d30fb13b6b69
xueyinglong <xueyinglong@bbdservice.com>
2019-12-11 11:31:35 +0800

    1. bug

--- src/background.js
+++ src/background.js
@@ -1,10 +1,3 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
-//example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
   chrome.pageAction.show(sender.tab.id);
   if (request.message == 'success') {
@@ -24,11 +17,3 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
   }
   sendResponse();
 });
-// const partUrl = 'http://it.bbdservice.com:8988/man-hour/admin';
-
-// chrome.webRequest.onCompleted.addListener(
-//     function (details) {
-//         console.log(details);
-//     },
-//     { urls: [partUrl + '/workTypeLink/getWorkTypes.html'] }
-// );
